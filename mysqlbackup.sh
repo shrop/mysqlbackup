520e0098231f9a558336844d89e70d3a173d9b17
Mark Shropshire <mdshrops@shropnet.net>
2011-03-20 21:08:08 -0400

    Initial commit

---
+++ mysqlbackup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash                                                                                                                                             
+
+# Backup mysql databases.
+
+# Check to see if mysqld is running.
+if ps ax | grep -v grep | grep mysqld > /dev/null
+	then
+		# Set a value that we can use for a datestamp
+		DATE=`date +%Y-%m-%d-%H%M%S`                                                                                                                                            
+		# Our Base backup directory
+		BASEBACKUP="."
+
+		for DATABASE in `cat dblist.txt`
+		do
+        	# This is where we throw our backups.
+        	FILEDIR="$BASEBACKUP/$DATABASE"
+
+        	# Test to see if our backup directory exists. 
+        	# If not, create it.
+        	if [ ! -d $FILEDIR ]
+        		then
+              		mkdir -p $FILEDIR
+       		fi
+
+       		echo -n "Exporting database:  $DATABASE"
+       		mysqldump --host=localhost --user=root --password=root --opt $DATABASE | gzip -c -9 > $FILEDIR/$DATABASE-$DATE.sql.gz
+       		echo "      ......[ Done ] "
+		done
+fi
+	
+# AutoPrune our backups.  This will find all files
+# that are "MaxFileAge" days old and delete them.
+MaxFileAge=3
+find $BASEBACKUP -name '*.gz' -type f -mtime +$MaxFileAge -exec rm -f {} \; 
